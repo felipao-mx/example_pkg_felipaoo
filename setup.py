10ccb1a1f572e2ee9112b513b5251ec3df6de628
Felipe López <flh.1989@gmail.com>
2018-11-01 15:11:36 -0600

    m

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="example_pkg_felipaoo",
-    version="0.0.1.7",
+    version="0.0.1.9",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
